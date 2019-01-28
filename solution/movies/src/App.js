011a632ed5ebcb7ab4eb1837c6330f392d6c00c4
hilal aissani <hilalaissani@gmail.com>
2019-01-27 19:19:50 -0500

    start app

--- solution/movies/src/App.js
+++ solution/movies/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./css/App.css";
 import AllMovies from "./allmovies/allmovies";
 class App extends Component {
   render() {
--- solution/movies/src/allmovies/allmovies.js
+++ solution/movies/src/allmovies/allmovies.js
@@ -34,79 +34,83 @@ class AllMovies extends Component {
 
     return (
       <div>
-        <h1> Welcome to Movies guide </h1>
-        <button
-          onClick={() => {
-            this.setState({
-              clickPopular: true,
-              clickPlaying: false,
-              clickTop: false
-            });
-          }}
-        >
-          Popular Movies
-        </button>
-        <button
-          onClick={() => {
-            this.setState({
-              clickPopular: false,
-              clickPlaying: true,
-              clickTop: false
-            });
-          }}
-        >
-          Playing Movies
-        </button>
-        <button
-          onClick={() => {
-            this.setState({
-              clickPopular: false,
-              clickPlaying: false,
-              clickTop: true
-            });
-          }}
-        >
-          Top Movies
-        </button>
-        {this.state.clickPopular &&
-          popularMovies.map((m, i) => {
-            return (
-              <div className="movie-card">
-                <h3 key={i}>{m.title}</h3>
-                <img
-                  src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
-                  width="400"
-                  height="400"
-                />
-              </div>
-            );
-          })}
-        {this.state.clickTop &&
-          topRatedMovies.map((m, i) => {
-            return (
-              <div>
-                <h3 key={i}>{m.title}</h3>
-                <img
-                  src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
-                  width="400"
-                  height="400"
-                />
-              </div>
-            );
-          })}
-        {this.state.clickPlaying &&
-          playingNowMovies.map((m, i) => {
-            return (
-              <div>
-                <h3 key={i}>{m.title}</h3>
-                <img
-                  src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
-                  width="400"
-                  height="400"
-                />
-              </div>
-            );
-          })}
+        <div className="nav-bar">
+          <h1> Welcome to Movies guide </h1>
+          <button
+            onClick={() => {
+              this.setState({
+                clickPopular: true,
+                clickPlaying: false,
+                clickTop: false
+              });
+            }}
+          >
+            Popular Movies
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                clickPopular: false,
+                clickPlaying: true,
+                clickTop: false
+              });
+            }}
+          >
+            Playing Movies
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                clickPopular: false,
+                clickPlaying: false,
+                clickTop: true
+              });
+            }}
+          >
+            Top Movies
+          </button>
+        </div>
+        <div className="all-movies">
+          {this.state.clickPopular &&
+            popularMovies.map((m, i) => {
+              return (
+                <div className="movie-card">
+                  <h3 key={i}>{m.title}</h3>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
+                    width="400"
+                    height="400"
+                  />
+                </div>
+              );
+            })}
+          {this.state.clickTop &&
+            topRatedMovies.map((m, i) => {
+              return (
+                <div className="movie-card">
+                  <h3 key={i}>{m.title}</h3>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
+                    width="400"
+                    height="400"
+                  />
+                </div>
+              );
+            })}
+          {this.state.clickPlaying &&
+            playingNowMovies.map((m, i) => {
+              return (
+                <div className="movie-card">
+                  <h3 key={i}>{m.title}</h3>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w1280${m.poster_path}`}
+                    width="400"
+                    height="400"
+                  />
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
